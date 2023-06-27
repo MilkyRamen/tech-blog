e6708c76db44ebe60f6275ffb1ba71891e7be717
MilkyRamen <127574535+MilkyRamen@users.noreply.github.com>
2023-06-27 19:14:35 -0400

    update app.js and create routes folder

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.set('views', path.join(__dirname, 'views'));
